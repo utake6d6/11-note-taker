9f4e29c09c30f11222b2baad43b614fe0d880c82
Bert <robertgill82@gmail.com>
2020-12-13 13:57:17 -0700

    pseudocode cleanup

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,42 +1,24 @@
-// <!-- The application should have a db.json file on the back end that will be used to store and retrieve notes using the fs module. -->
 const fs = require("fs");
 const express = require("express");
 const app = express();
-// npm module for unique ids
+
 const { v4: uuidv4 } = require("uuid");
 
-// GET /api/notes - should read the db.json file and return all saved notes as JSON.
-//ReferenceError: app is not defined at Object.<anonymous> (C:\Users\berto\Desktop\projects\11-note-taker\routes\apiRoute.js:4:1)
 app.get("/notes", (req, res) => {
   let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
   res.json(notes);
 });
 
-// POST /api/notes - should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
 app.post("/notes", (req, res) => {
-  // receive a new note
   const newNote = req.body;
   console.log(JSON.stringify(newNote));
-
-  // get a newNote
   newNote.id = uuidv4();
-
-  // add it to the db.json file
   let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
-  // collection.push() appends one or more element(s) to the end of the collection instance and returns the edited collection
   notes.push(newNote);
-  // save on the request body
+
   fs.writeFileSync("./db/db.json", JSON.stringify(notes));
   console.log("New note added.");
-  // return the new note to the client
   res.json(notes);
-});
-
-// DELETE /api/notes/:id - should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
-app.delete("/notes/:id", (req, res) => {
-  // read all notes from the db.json file
-  // remove the note with the given id property
-  // then rewrite the notes to the db.json file
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 const express = require("express");
 
-// initialize express
 const apiRoute = require("./routes/apiRoute");
 const htmlRoute = require("./routes/htmlRoute");
 const app = express();
 
-// creates port
 const PORT = process.env.PORT || 3001;
 
-// middleware:
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
-// files downloaded from the server, create new directory, public
 app.use(express.static("public"));
 
-// routes
 app.use("/api", apiRoute);
 app.use("/", htmlRoute);
 
-// listener:
 app.listen(PORT, () => {
   console.log(`App listening on PORT ${PORT}`);
 });
