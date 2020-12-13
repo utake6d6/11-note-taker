70fb746b9ef7aaa6c7edad31608a2f37120badfe
Bert <robertgill82@gmail.com>
2020-12-13 13:32:24 -0700

    heroku site

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -32,7 +32,7 @@ app.post("/notes", (req, res) => {
   res.json(notes);
 });
 
-// DELETE /api/notes/:id - should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file. -->
+// DELETE /api/notes/:id - should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 app.delete("/notes/:id", (req, res) => {
   // read all notes from the db.json file
   // remove the note with the given id property
